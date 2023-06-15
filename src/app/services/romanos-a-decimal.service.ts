5b024f522ed6d401236cc90be22cb8524ef127ca
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-15 09:28:51 -0300

    Regex agregado, solo se aceptan numeros romanos

--- src/app/services/romanos-a-decimal.service.ts
+++ src/app/services/romanos-a-decimal.service.ts
@@ -22,11 +22,10 @@ export class RomanosADecimalService {
     ];
 
     //Restricciones
-    /*
-    if ( /^( miles )( centenas )( decenas )( unidades )$/.test(num) ) {
-      return ""
+    //      MILES      CENTENAS        DECENAS         UNIDADES
+    if (!/^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(num) ) {
+      return 0
     }
-    */
 
     //recorre el string num
     for (let i = 0; i < num.length; i++) {
